ff6232a1398c419fc1bd3bbeb422825ab22ee207
Karan Kangude <karankangude17@gmail.com>
2020-04-18 11:48:19 +0530

    reset password pattern validation

--- src/app/components/reset-pass/old-password.validators.ts
+++ src/app/components/reset-pass/old-password.validators.ts
@@ -1,15 +1,20 @@
 import { LoginComponent } from './../../pages/login/login.component';
-import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 export class OldPwdValidators {
 
-  static shouldBeOldOne(control: AbstractControl): Promise<ValidationErrors | null> {
-    return new Promise((resolve, reject) => {
-        if (control.value !== 'kirana') {
-          resolve({ shouldBeOldOne: true });
-        } else {
-          resolve(null);
-        }
-    });
+  static patternValidator(regex: RegExp, error: ValidationErrors): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (!control.value) {
+        // if control is empty return no error
+        return null;
+      }
+
+      // test the value of the control against the regexp supplied
+      const valid = regex.test(control.value);
+
+      // if true, return no error (no error), else return error passed in the second parameter
+      return valid ? null : error;
+    };
   }
 
   static matchPwds(control: AbstractControl) {
