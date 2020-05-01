29c9574158b2342290b892cb4de5c20c749df331
Karan Kangude <karankangude17@gmail.com>
2020-05-01 20:00:47 +0530

    invite request btn changes done

--- src/app/components/invite-request/invite-request.component.ts
+++ src/app/components/invite-request/invite-request.component.ts
@@ -1,68 +1,14 @@
-import { Component, OnInit, Input, Inject } from "@angular/core";
-import { RetailerService } from "../../../app/services/retailer.service";
-import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from "@angular/material";
-import { Router } from "@angular/router";
+import { Component, OnInit, Input, Inject } from '@angular/core';
+import { RetailerService } from '../../../app/services/retailer.service';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog, MatSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: "app-invite-request",
-  templateUrl: "./invite-request.component.html",
-  styleUrls: ["./invite-request.component.scss"],
+  // tslint:disable-next-line: component-selector
+  selector: 'show-invite-detail-dialogue',
+  templateUrl: './showInviteDetailModal.html',
 })
-export class InviteRequestComponent implements OnInit {
-  constructor(
-    private retailerService: RetailerService,
-    public dialog: MatDialog,
-    private router: Router
-  ) { }
-
-  @Input() inviteRequest;
-
-  ngOnInit() { }
-
-  approveInviteRequest() {
-    let data = { requestId: this.inviteRequest.id, approved: true };
-    this.retailerService.inviteRequestResponse(data).subscribe((result) => {
-      alert("Request Approved");
-      this.router
-        .navigateByUrl("/login", { skipLocationChange: true })
-        .then(() => {
-          this.router.navigate(["/dashboard"]);
-        });
-    });
-  }
-
-  rejectInviteRequest() {
-    let data = { requestId: this.inviteRequest.id, approved: false };
-    this.retailerService.inviteRequestResponse(data).subscribe((result) => {
-      alert("Request Rejected");
-      this.router
-        .navigateByUrl("/login", { skipLocationChange: true })
-        .then(() => {
-          this.router.navigate(["/dashboard"]);
-        });
-    });
-  }
-
-  openDialog() {
-    const dialogRef = this.dialog.open(ShowInviteDetailModal, {
-      width: "90%",
-      maxWidth: "450px",
-      data: this.inviteRequest,
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      if (result == true)
-        this.approveInviteRequest();
-      else if (result == false)
-        this.rejectInviteRequest();
-    });
-  }
-}
-
-@Component({
-  selector: "show-invite-detail-dialogue",
-  templateUrl: "./showInviteDetailModal.html",
-})
+// tslint:disable-next-line: component-class-suffix
 export class ShowInviteDetailModal {
   constructor(
     public dialogRef: MatDialogRef<ShowInviteDetailModal>,
@@ -73,3 +19,65 @@ export class ShowInviteDetailModal {
     this.dialogRef.close();
   }
 }
+@Component({
+  selector: 'app-invite-request',
+  templateUrl: './invite-request.component.html',
+  styleUrls: ['./invite-request.component.scss'],
+})
+export class InviteRequestComponent implements OnInit {
+  constructor(
+    private retailerService: RetailerService,
+    public dialog: MatDialog,
+    private router: Router,
+    private snackbar: MatSnackBar,
+  ) { }
+
+  @Input() inviteRequest;
+
+  ngOnInit() { }
+
+  approveInviteRequest() {
+    const data = { requestId: this.inviteRequest.id, approved: true };
+    this.retailerService.inviteRequestResponse(data).subscribe((result) => {
+      this.snackbar.open('Request Approved', '', {
+        duration: 2000,
+      });
+      this.router
+        .navigateByUrl('/login', { skipLocationChange: true })
+        .then(() => {
+          this.router.navigate(['/dashboard']);
+        });
+    });
+  }
+
+  rejectInviteRequest() {
+    const data = { requestId: this.inviteRequest.id, approved: false };
+    this.retailerService.inviteRequestResponse(data).subscribe((result) => {
+      this.snackbar.open('Request Rejected', '', {
+        duration: 2000,
+      });
+      this.router
+        .navigateByUrl('/login', { skipLocationChange: true })
+        .then(() => {
+          this.router.navigate(['/dashboard']);
+        });
+    });
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(ShowInviteDetailModal, {
+      width: '90%',
+      maxWidth: '450px',
+      data: this.inviteRequest,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result == true) {
+        this.approveInviteRequest();
+      } else if (result == false) {
+        this.rejectInviteRequest();
+      }
+    });
+  }
+}
+
