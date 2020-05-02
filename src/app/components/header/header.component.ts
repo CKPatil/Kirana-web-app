9f4f21341aba884a5906cc4eefe036df7bb4f280
Karan Kangude <karankangude17@gmail.com>
2020-05-03 01:23:07 +0530

    changes in header accordingly

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,6 +4,7 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog
 import { ThemePalette } from '@angular/material/core';
 import { NotificationComponent} from './../notification/notification.component';
 import { ResetPassComponent } from './../reset-pass/reset-pass.component';
+import { TransactionService } from './../../services/transaction.service';
 export interface DialogData {
   animal: string;
   name: string;
@@ -18,16 +19,36 @@ export class HeaderComponent implements OnInit {
   animal: string;
   name: string;
   change: any;
-  constructor(private router: Router, public dialog: MatDialog) { }
+  notification = [];
+  constructor(private router: Router,
+              public dialog: MatDialog,
+              private transactionService: TransactionService) { }
 
   ngOnInit() {
     this.change = localStorage.getItem('change');
     this.repeat();
+
   }
   repeat() {
     setInterval( () => {
       this.change = localStorage.getItem('change');
     }, 3000);
+  }
+  getAllNotifications() {
+    setInterval ( () => {
+      this.transactionService.observeOrders.subscribe((data) => {
+        this.notification = data;
+        for (let i = 0 ; i < this.notification.length ; i++ ) {
+          // tslint:disable-next-line: radix
+          if ((this.notification[i].status_change.getTime() - parseInt(localStorage.getItem('previousVisited')))
+              > 0 && this.notification[i].status !== 'Ordered') {
+            this.change = true;
+            localStorage.setItem('change', this.change);
+            break;
+          }
+        }
+      });
+    }, 60000);
   }
   logout() {
     localStorage.clear();
