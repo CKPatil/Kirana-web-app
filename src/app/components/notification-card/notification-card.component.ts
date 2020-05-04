3efd7d6f71a93d138fb32671332aa808be3b6ee9
Karan Kangude <karankangude17@gmail.com>
2020-05-04 17:51:02 +0530

    new notification card component

---
+++ src/app/components/notification-card/notification-card.component.ts
@@ -0,0 +1,21 @@
+import { TransactionService } from './../../services/transaction.service';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-card',
+  templateUrl: './notification-card.component.html',
+  styleUrls: ['./notification-card.component.scss']
+})
+export class NotificationCardComponent implements OnInit {
+  @Input() notification: any;
+  constructor(private transactionService: TransactionService) { }
+
+  ngOnInit(): void {
+  }
+  onClick(orderId) {
+    this.transactionService.readNotification(orderId)
+      .subscribe( (res) => {
+        console.log(res);
+      });
+  }
+}
