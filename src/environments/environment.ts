b495306212d5742711ce47e59d2c5d4d831adb4b
Sachin Kumar Rajput <skr571999@gmail.com>
2020-05-12 21:03:54 +0530

    - change the IP

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  backend_end_point: 'http://13.233.33.65/',
+  backend_end_point: 'https://kiranamysore.com/',
   port_number: '8000',
   userDetails: 'api/v1/userDetails/',
   order: "api/v1/vendor/order/",
